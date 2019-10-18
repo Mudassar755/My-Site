836ba92f2b506fa0313bc57061950ee4b0d8e3ad
mudassar755 <mudasar.se@gmail.com>
2019-10-18 15:23:23 +0500

    did some changes

--- src/components/pages/landing/VideoSection.js
+++ src/components/pages/landing/VideoSection.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 //react-bootstrap
 import { Container, Row, Col, Modal } from "react-bootstrap";
 
@@ -9,31 +9,36 @@ function VideoSection() {
   const handleShow = () => setShow(true);
   return (
     <div>
-    <section id="video-play">
-      <div className="dark-overlay">
-        <Row>
-          <Col>
-            <Container className="p-5">
-              <a href="!#" onClick = {handleShow}
-                className="video"
-              >
-                <i className="fas fa-play fa-3x"></i>
-              </a>
-              <h1>See What We Do</h1>
-            </Container>
-          </Col>
-        </Row>
-      </div>
-    </section>
+      <section id="video-play">
+        <div className="dark-overlay">
+          <Row>
+            <Col>
+              <Container className="p-5">
+                <a href="#" onClick={handleShow} className="video">
+                  <i className="fas fa-play fa-3x"></i>
+                </a>
+                <h1>See What We Do</h1>
+              </Container>
+            </Col>
+          </Row>
+        </div>
+      </section>
 
-{/* Video Modal */}
+      {/* Video Modal */}
 
-    <Modal show={show} onHide={handleClose}>
-        
-        <Modal.Body><iframe src="https://www.youtube.com/embed/MjxGwfa5lxw" frameborder="0" title="Video uploading..." height="350" width="100%" allowfullscreen></iframe></Modal.Body>
-        
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+          <iframe
+            src="https://www.youtube.com/embed/MjxGwfa5lxw"
+            frameborder="0"
+            title="Video uploading..."
+            height="350"
+            width="100%"
+            allowfullscreen
+          ></iframe>
+        </Modal.Body>
       </Modal>
-      </div>
+    </div>
   );
 }
 
